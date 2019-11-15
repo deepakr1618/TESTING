5e7aba43989bdf4bdd45bfb602744606bf7d412c
--replace-all <deepak.r1618@gmail.com>
2019-11-15 16:43:08 +0530

    Added id in url

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express =require('express')
 const app = express()
 
-app.get("/",(req,res)=>{
-  res.send("Working!")
+app.get("/:id",(req,res)=>{
+  res.send("Hello there "+req.params.id)
 })
 
 
